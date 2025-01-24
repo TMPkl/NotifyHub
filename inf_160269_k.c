62e28f93141a84bdab092c5880e27ae5193edf8a
TMPkl <karol.leszynski1@gmail.com>
2025-01-24 15:21:42 +0100

    klient test

---
+++ inf_160269_k.c
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+#include "myStructs.h" 
+#include <stdbool.h>
+#include <unistd.h>
+
+
+#define INITIAL_COMUNICATION_KEY 123456
+
+int my_id;
+struct init_client client;
+struct 
+
+void init_producer()
+{   int init_id = msgget(INITIAL_COMUNICATION_KEY, IPC_CREAT | 0644);
+    printf("Podaj pod jakie chcesz mieć id: \n");
+    scanf("%d", &my_id);
+    client.id_client = my_id;
+    client.type = INITIAL_CLIENT_DISTRIBUTOR_CHANEL;
+    msgsnd(init_id, &client, sizeof(client) - sizeof(long), 0);
+    struct producent_distributor_feedback feedback;
+    msgrcv(init_id, &feedback, sizeof(feedback) - sizeof(long), INITIAL_CLIENT_DISTRIBUTOR_FEEDBACK, 0);
+    if (feedback.status == -1)
+    {
+        printf("Podane id jest nieprawidłowe, nie udało się połączć.\n");
+        return;
+    }
+    
+
+    else{
+        return;
+    }
+
+
+}
+
+
+int main(){
+    printf("######Klient:\n");
+
+
+
+
+
+
+
+}
